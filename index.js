dbedec9a200e14a8d91ff66f558ffcd594106582
Nick Camillo <nick.camillo@gmail.com>
2015-02-13 10:26:39 -0500

    grid size based off vars

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 // Vars
-var size = 50;
-
+var gridSize = 30;
+var pixelSize = 15;
 // Build Grid
-$( document ).ready(function() {
+var buildGrid = function(gridSize) { 
   // Build container
   $('body').append("<div id='grid-container'></div>");
-  for (var row = 0; row < size; row++) {
+  for (var row = 1; row < gridSize + 1; row++) {
+    
     // Make unique id for each row and render
-    var rowString = "<div class='row' id='row" + (row + 1) + "'></div>";
+    var rowString = "<div class='row' id='row" + row + "'></div>";
     console.log(rowString);
     $('#grid-container').append(rowString);
     
     // Make unique id for each pixel and render
-    for (var i = 0; i < (size); i++) {
+    for (var i = 1; i < gridSize + 1; i++) {
       var pixelString = "<div class='pixel' id='pixel" + i + "'></div>";
-      $('#row' + (i +1)).append(pixelString);
+      $('#row' + row).append(pixelString);
     }
   }       
+
+  // Set container width to width of all pixels
+  $('#grid-container').css("width", gridSize * (pixelSize+1));
+};  
+// Build color swatch
+
+// Initialize App
+
+$( document ).ready(function() {
+  buildGrid(gridSize);
 });
